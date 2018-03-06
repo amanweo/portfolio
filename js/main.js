e5dd893b12a5c6cbf996c18664ee1f9ee1579b3e
amanweo <amandeep@webexpertsonline.info>
2018-03-06 18:18:16 +0530

    add latest updates

--- js/main.js
+++ js/main.js
@@ -24,20 +24,22 @@ app.controller('myCtrl', function($scope){
 //    }
 });
 app.controller('categ', function($scope){    
-   $scope.categ = {category: [
-                        {categName:"Single tracks"},
-                        {categName:"All Music"},
-                        {categName:"Punjabi Music"},
-                        {categName:"Bollywood Music"},
-                        {categName:"Singer Collection"},
-                        {categName:"Punjabi Videos"},
-                        {categName:"Bollywood Videos"}
-                        ],
-                        updateList: [
-                         {upList:"abc"},
-                         {upList:"abc"},
-                         {upList:"abc"},
-                    ]
+   $scope.categ = {
+       category: [
+                {categName:"Single tracks"},
+                {categName:"All Music"},
+                {categName:"Punjabi Music"},
+                {categName:"Bollywood Music"},
+                {categName:"Singer Collection"},
+                {categName:"Punjabi Videos"},
+                {categName:"Bollywood Videos"}
+                ],
+                updateList: [
+                 {upList:"Single Track", trackName:"Track 1", trackName2:"Track 2", trackName3:"Track 3",artist1:"Artist 1", artist2:"Artist 2", artist3:"Artist 3", artist4:"Artist 4", artist5:"Artist 5"},
+                 {upList:"Punjabi Album", trackName:"Album 1", trackName2:"Album 2", trackName3:"Album 3",artist1:"Artist 1", artist2:"Artist 2", artist3:"Artist 3", artist4:"Artist 4", artist5:"Artist 5"},
+                 {upList:"Bollywood Album", trackName:"Album 1", trackName2:"Album 2", trackName3:"Album 3",artist1:"Artist 1", artist2:"Artist 2", artist3:"Artist 3", artist4:"Artist 4", artist5:"Artist 5"}
+            ]
+                
     }
 })
 
