29e8ee3d0ebbc450c99c4f7700a50e803b204a0d
amanweo <amandeep@webexpertsonline.info>
2018-03-16 09:49:41 +0530

    second commit

--- js/main.js
+++ js/main.js
@@ -12,36 +12,20 @@ app.config(function($routeProvider){
     });
 });
 
+
 app.controller('myCtrl', function($scope){
-    $scope.list = [
-                   {sr:"3", name: "Demo", lastname: "Testing"},
-                    {sr:"1", name: "Amandeep", lastname: "Singh"},
-                   {sr:"2", name: "Testing", lastname: "Testing"}
-                  ];    
-    
-//    $scope.orderByMe = function(x){
-//        $scope.sortlist = x;
-//    }
+   $scope.info = { contact: [
+                            {email:"demo@gmail.com", phone:"+91-9876543210", location:"#123 Lorem Ipsum"}
+                            ],
+                        personal: [
+                            {name:"John Smith", bio:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id laoreet urna, quis mattis quam. Morbi tellus velit, cursus eu mattis vitae, dapibus non justo. In facilisis aliquam magna, a lacinia nunc dapibus vitae. Phasellus sit amet bibendum nulla. Morbi euismod et mauris eu lacinia. Nulla ut sapien sit amet quam posuere porta ut eget nisi. In eu elit porttitor, elementum lorem in, tempor velit. In in viverra neque. Sed tempor sollicitudin erat quis consectetur. Donec semper consectetur ipsum, nec blandit massa. Maecenas consequat ipsum ac neque eleifend, id luctus nibh sollicitudin. In ultricies nisl id augue scelerisque, sollicitudin hendrerit dui euismod. Curabitur hendrerit augue quis placerat consequat. Pellentesque eget arcu id neque semper bibendum ut non tortor. Aliquam aliquam tortor sed magna gravida viverra ac a justo. Proin finibus justo id efficitur iaculis. Fusce dignissim nisl justo, ac gravida neque hendrerit at.", fname: "Villiam Smith", dob:"07/11/1990", gender:"Male", language:"Punjabi, Hindi, English"}
+                        ],
+                  education: [
+                            {course1: "Bachelor of Computer Applications (BCA)", course2: "Masters of Computer Applications (MCA)", course3: "6 Months professional training", institute1: "Chandigarh Groups Of Colleges Landran", institute2: "Distance Learning (PTU)", institute3: "Morph Academy Chandigarh"  }
+                  ],
+                  
+                    }
 });
-app.controller('categ', function($scope){    
-   $scope.categ = {
-       category: [
-                {categName:"Single tracks"},
-                {categName:"All Music"},
-                {categName:"Punjabi Music"},
-                {categName:"Bollywood Music"},
-                {categName:"Singer Collection"},
-                {categName:"Punjabi Videos"},
-                {categName:"Bollywood Videos"}
-                ],
-                updateList: [
-                 {upList:"Single Track", trackName:"Track 1", trackName2:"Track 2", trackName3:"Track 3",artist1:"Artist 1", artist2:"Artist 2", artist3:"Artist 3", artist4:"Artist 4", artist5:"Artist 5"},
-                 {upList:"Punjabi Album", trackName:"Album 1", trackName2:"Album 2", trackName3:"Album 3",artist1:"Artist 1", artist2:"Artist 2", artist3:"Artist 3", artist4:"Artist 4", artist5:"Artist 5"},
-                 {upList:"Bollywood Album", trackName:"Album 1", trackName2:"Album 2", trackName3:"Album 3",artist1:"Artist 1", artist2:"Artist 2", artist3:"Artist 3", artist4:"Artist 4", artist5:"Artist 5"}
-            ]
-                
-    }
-})
 
 
 
